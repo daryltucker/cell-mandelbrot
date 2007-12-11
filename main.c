68fc86cfd4dbbad3bf877a384b625c0527d80d5b
tkjantti <tkjantti@95fa8579-963f-0410-b59e-35acc10d8b93>
2007-12-11 14:10:56 +0000

    Pääohjelman muotoilua...

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <libspe2.h>
 
@@ -13,7 +14,7 @@
 #define MAX_DATA_PER_THREAD 16384
 
 
-// extern spe_program_handle_t fractal_handle;
+// extern spe_program_handle_t mandelbrot_handle;
 
 
 typedef struct {
@@ -28,7 +29,11 @@ void *run_spu_thread(void *arg)
     unsigned int entry = SPE_DEFAULT_ENTRY;
     args = *((thread_arguments *) arg);
 
-    // K‰ynnistet‰‰n s‰ie...
+    if ( spe_context_run(args.context, &entry, 0,
+			 &foo_bar, NULL, NULL) < 0 ) {
+	perror("SPE kontekstin k‰ynnistys ei onnistunut");
+	exit(1);
+    }
 
     pthread_exit(NULL);
 }
@@ -37,14 +42,44 @@ void *run_spu_thread(void *arg)
 int main(int argc, char *argv[])
 {
     int img_width = 300, img_height = 300;
+    int i, spu_threads;
+    thread_arguments thread_args[MAX_SPU_THREADS];
+    pthread_t threads[MAX_SPU_THREADS];
 
-    if (spe_cpu_info_get(SPE_COUNT_USABLE_SPES,-1) < 1){
+    if ((spu_threads = spe_cpu_info_get(SPE_COUNT_USABLE_SPES, -1)) < 1){
 	fprintf(stderr, "Ei ole vapaata SPE-ydint‰\n");
 	exit(1);
     }
 
+    if (spu_threads > MAX_SPU_THREADS) spu_threads = MAX_SPU_THREADS;
+
+    // Varataan muistia kuvaa varten...
+
     // Ladataan spu-konteksteja ja
     // k‰ynnistell‰‰n s‰ikeit‰...
+
+    for (i=0; i<spu_threads; i++)
+    {
+	thread_arguments *arg = &thread_args[i];
+
+	// Asetetaan s‰ikeelle osoitin kuvaan
+	// sek‰ omaan osuuteen kuvasta...
+
+	if ((arg->context = spe_context_create(0, NULL)) == NULL) {
+	    perror("Kontekstin luonti ei onnistunut");
+	    exit(1);
+	}
+
+	// T‰ss‰ ladataan spu-kontekstia...
+
+	// ja luodaan s‰ie...
+    }
+
+    // SPE:t laskee kovasti...
+
+    // Joinataan...
+
+    // Tuhotaan s‰ikeet...
 
     // Piirret‰‰n lopputulos (tiedostoon ja/tai ikkunaan)
 
