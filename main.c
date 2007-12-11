e26f445f1d8230948c657fd527864d0504d063d5
tkjantti <tkjantti@95fa8579-963f-0410-b59e-35acc10d8b93>
2007-12-11 12:51:32 +0000

    alkua

--- main.c
+++ main.c
@@ -4,10 +4,38 @@
  * Tero Jäntti, Matti Lehtinen 2007.
  */
 
+#include <stdio.h>
 #include <pthread.h>
+#include <libspe2.h>
+
+
+extern spe_program_handle_t convert_to_upper_handle;
+
+spe_context_ptr_t speid;
+unsigned int entry = SPE_DEFAULT_ENTRY;
 
 
 int main(int argc, char *argv[])
 {
+    int img_width = 300, img_height = 300;
+
+    if (spe_cpu_info_get(SPE_COUNT_USABLE_SPES,-1) < 1){
+	fprintf(stderr, "Ei ole vapaata SPE-ydintä\n");
+	exit(1);
+    }
+
+    if ((speid = spe_context_create(0,NULL)) == NULL) {
+	fprintf(stderr, "Kontekstin luonti epäonnistui\n");
+	exit(1);
+    }
+
+    // Ladataan SPE-ohjelma kontekstiin...
+
+    // Suoritetaan SPE-ohjelma kontekstissa...
+
+    // Tuhotaan konteksti...
+
+    // Piirretään lopputulos (tiedostoon ja/tai ikkunaan)
+
     return 0;
 }
