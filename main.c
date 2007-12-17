ff2692ef8fd42514549b2432277eb189c99bb647
tkjantti <tkjantti@95fa8579-963f-0410-b59e-35acc10d8b93>
2007-12-17 12:47:57 +0000

    SPU-kontekstien latailua ja tuhoamista

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #define MAX_DATA_PER_THREAD 16384
 
 
-// extern spe_program_handle_t mandelbrot_handle;
+extern spe_program_handle_t mandelbrot_handle;
 
 
 typedef struct {
@@ -23,17 +23,21 @@ typedef struct {
 } thread_arguments;
 
 
+void fail(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+
 void *run_spu_thread(void *arg)
 {
     thread_arguments args;
     unsigned int entry = SPE_DEFAULT_ENTRY;
     args = *((thread_arguments *) arg);
 
-/*     if ( spe_context_run(args.context, &entry, 0, */
-/* 			 &foo_bar, NULL, NULL) < 0 ) { */
-/* 	perror("SPE kontekstin k‰ynnistys ei onnistunut"); */
-/* 	exit(1); */
-/*     } */
+    if ( spe_context_run(args.context, &entry, 0, &foo_bar, NULL, NULL) < 0 )
+	fail("SPE kontekstin k‰ynnistys ei onnistunut");
 
     puts("Hei, olen s‰ie ja kuolen ihan kohta!");
 
@@ -55,10 +59,9 @@ int main(int argc, char *argv[])
 
     if (spu_threads > MAX_SPU_THREADS) spu_threads = MAX_SPU_THREADS;
 
-    // Varataan muistia kuvaa varten...
+    spu_threads = 4;
 
-    // Ladataan spu-konteksteja ja
-    // k‰ynnistell‰‰n s‰ikeit‰...
+    // Varataan muistia kuvaa varten...
 
     for (i=0; i<spu_threads; i++)
     {
@@ -67,14 +70,13 @@ int main(int argc, char *argv[])
 	// Asetetaan s‰ikeelle osoitin kuvaan
 	// sek‰ omaan osuuteen kuvasta...
 
-/* 	if ((arg->context = spe_context_create(0, NULL)) == NULL) { */
-/* 	    perror("Kontekstin luonti ei onnistunut"); */
-/* 	    exit(1); */
-/* 	} */
+	if ((arg->context = spe_context_create(0, NULL)) == NULL)
+	    fail("Kontekstin luonti ei onnistunut");
 
 	// T‰ss‰ ladataan spu-kontekstia...
+	if ( spe_program_load(arg->context, &mandelbrot_handle) != 0 )
+	    fail("SPU-ohjelman lataus ei onnistunut");
 
-	// ja luodaan s‰ie...
 	pthread_create(&threads[i],
 		       NULL,
 		       &run_spu_thread,
@@ -84,16 +86,14 @@ int main(int argc, char *argv[])
     // SPE:t laskee kovasti...
     puts("P‰‰ohjelma odottelee s‰ikeit‰...");
 
-    // Joinataan...
     for (i=0; i<spu_threads; i++)
     {
-	if (pthread_join(threads[i], NULL)) {
-	    perror("pthread_join ep‰onnistui");
-	    exit(1);
-	}
-    }
+	if (pthread_join(threads[i], NULL))
+	    fail("pthread_join() ep‰onnistui");
 
-    // Tuhotaan s‰ikeisiin liitetyt kontekstit...
+	if (spe_context_destroy(thread_args[i].context))
+	    fail("Kontekstin tuhoaminen ep‰onnistui");
+    }
 
     // Piirret‰‰n lopputulos (tiedostoon ja/tai ikkunaan)
 
