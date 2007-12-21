22729a6fb69f51a0b39bbfc2ceaa2ab45ee0304d
tkjantti <tkjantti@95fa8579-963f-0410-b59e-35acc10d8b93>
2007-12-21 11:15:33 +0000

    Parametrien välitystä SPU:lle

--- trunk/spu_fractal_driver.c
+++ trunk/spu_fractal_driver.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "fractal.h"
 #include <spu_intrinsics.h>
 #include <spu_mfcio.h>
+#include <stdio.h>
 
 typedef unsigned long long uint64;
 typedef unsigned int uint32;
 
 
-int main(uint64 spe_id, uint64 conversion_info_ea)
+int main(uint64 spe_id, uint64 fractal_parameter_ea)
 {
-    puts("SPU: Elama on!");
+    fractal_parameters parameters;
+
+    // Ladataan parametrit...
+    mfc_write_tag_mask(1<<0);  //DMA-tunniste = 0
+    mfc_get(&parameters, fractal_parameter_ea, sizeof(fractal_parameters), 0, 0, 0);
+    spu_mfcstat(MFC_TAG_UPDATE_ALL); // Ja odotellaan niitä.
+    
+    printf("Saatiin mm. width: %u ja area_x: %u\n", parameters.width, parameters.area_x);
     return 0;
 }
