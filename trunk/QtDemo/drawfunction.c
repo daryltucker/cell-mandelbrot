48475ad262e5acd701d9e3ad4696175503877f49
Pa77eri <Pa77eri@95fa8579-963f-0410-b59e-35acc10d8b93>
2008-01-09 16:30:38 +0000

    -Moved comments from a c-file to a header-file.

--- trunk/QtDemo/drawfunction.c
+++ trunk/QtDemo/drawfunction.c
@@ -4,25 +4,6 @@
 #define MIN(_A_, _B_) ((_A_ < _B_) ? _A_ : _B_)
 #define uint unsigned int
 
-//! A function that will draw a mandelbrot on a given buffer.
-/*! This function will draw a specified area of the mandelbrot fractal
-    to a given image. The image can also be given as portions of the 
-    whole image. This will provide a convenient way to draw the whole 
-    image in parallel.
-   \param width The width of the whole image.
-   \param height The height of the whole image.
-   \param reOffset The offset in the real-axes on a complex plane.
-   \param imOffset The offset in the imaginary-axes on a complex plane.
-   \param zoom The level of the magnification.
-   \param maxIteration The maximum count of iterations performed on 
-    each pixel.
-   \param areaBuffer A pointer to the beginning of the drawable area.
-   \param areaX The horizontal starting point on the whole image.
-   \param areaY The vertical starting point on the whole image.
-   \param areaWidth The width of the drawable area.
-   \param areaHeight The height of the drawable area.
-   \bytesPerPixel The amount of bytes reserved for one pixel.
- */
 void drawMandelbrotArea( uint width, uint height,
                          double reOffset, double imOffset,
                          double zoom, uint maxIteration,
