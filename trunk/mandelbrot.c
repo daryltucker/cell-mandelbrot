97b62c792cd6c3d8f98950594e6f49ba5d020a12
tkjantti <tkjantti@95fa8579-963f-0410-b59e-35acc10d8b93>
2008-02-29 11:06:10 +0000

    scale, offset_x ja offset_y lasketaan spu:n main:ssa

--- trunk/mandelbrot.c
+++ trunk/mandelbrot.c
@@ -2,25 +2,17 @@
 
 
 void drawMandelbrotArea( uint32 width, uint32 height,
-                         float reOffset, float imOffset,
-                         float zoom, uint32 maxIteration,
+                         float offsetX, float offsetY,
+                         float scale, uint32 maxIteration,
                          char *areaBuffer,
                          uint32 areaX, uint32 areaY,
                          uint32 areaWidth, uint32 areaHeight,
                          uint32 bytesPerPixel )
 {
   float x0, y0, x, y, xTemp, yTemp;
-  float scale, mandSize, offsetX, offsetY;
   unsigned int i, j, iteration, color, maxColor;
   signed int k;
   char *line;
-
-  mandSize = MANDELBROT_DEFAULT_SIZE / zoom;
-  scale = mandSize / MIN(width, height);
-  offsetX = mandSize / -2.0f * (width > height ? (float)width/height : 1.0f)
-            + reOffset;
-  offsetY = mandSize / -2.0f * (width < height ? (float)height/width : 1.0f)
-            + imOffset;
 
   for (j = areaY; j < areaHeight + areaY; j++)
   {
